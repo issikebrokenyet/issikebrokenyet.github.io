e22f4c16dbfbced9f2402acd2bf4910ca3ae2005
Luca De Feo <defeo@merry>
2022-08-21 02:35:43 +0200

    Reworking the subvariant architecture

--- src/build.py
+++ src/build.py
@@ -3,6 +3,7 @@ from jinja2 import Template, Environment, FileSystemLoader, select_autoescape
 from fractions import Fraction
 import markdown
 import re
+from functools import cached_property
 
 class L:
     """
@@ -20,7 +21,7 @@ class L:
         m = re.match(r'L\(([\d/]+)(,([\d/]+))?\)$', str)
         if m:
             return cls(m.group(1), m.group(3))
-        m = re.match(r'exp(\(([\d/]+)\))?$', str)
+        m = re.match(r'exp(\(([\d/.]+)\))?$', str)
         if m:
             return cls(1, m.group(2))
         raise RuntimeError('Cannot parse %s as a complexity' % str)
@@ -57,25 +58,49 @@ class L:
         else:
             return 'subexp'
 
+#----------------------------------------------#
+# A utility class to easily access subvariants #
+#----------------------------------------------#
+
+class NestedDict(dict):
+    def __getitem__(self, key):
+        if ">" in key:
+            parent, variant = key.split(">", maxsplit=1)
+            return self[parent].props["variants"][variant]
+        else:
+            return super().__getitem__(key)
+
 #----------------------------------#
 # Main Class for Rows in our table #
 #----------------------------------#
 
 class Entry:
-    def __init__(self, id, props):
+    def __init__(self, id, props, parent=None):
+        self.parent = parent
         self.props = props
         self.props["id"] = id
         self.props["this"] = self
-
+        if "variants" in self.props:
+            self.props["variants"] = NestedDict({
+                var: self.__class__(var, props, self)
+                for var, props in self.props["variants"].items()
+            })
+            
     def __repr__(self):
         return self.template.render(self.props)
 
+    @cached_property
+    def longid(self):
+        return (self.parent.longid + '>' if self.parent else '') + self.props["id"]
+    
+    @cached_property
     def name(self):
         if self.props['name'].get('long') and self.props['name'].get('short'):
             return f'<abbr title="{ self.props["name"]["long"] }">{ self.props["name"]["short"] }</abbr>'
         else:
             return self.props['name'].get('short') or self.props['name'].get('long')
 
+    @cached_property
     def reference(self):
         links = []
         reference_dict = self.props.get("references", {})
@@ -89,15 +114,15 @@ class Entry:
 
     def comment_checkbox(self, table):
         if self.props.get("comment"):
-            input_ele = f'<input type="checkbox" name="comment-checkbox" id="comment-{table}-{self.props["id"]}-checkbox">'
-            label_ele = f'<label for="comment-{table}-{self.props["id"]}-checkbox">Comments</label>'
+            input_ele = f'<input type="checkbox" name="comment-checkbox" id="comment-{table}:{self.longid}!checkbox">'
+            label_ele = f'<label for="comment-{table}:{self.longid}!checkbox">Comments</label>'
             return f"{input_ele}\n{label_ele}"
         return "-"
 
     def variant_checkbox(self, table):
         if self.props.get("variants"):
-            input_ele =  f'<input type="checkbox" name="variant-checkbox" id="variant-{table}-{self.props["id"]}-checkbox">'
-            label_ele = f'<label for="variant-{table}-{self.props["id"]}-checkbox">Variants</label>'
+            input_ele =  f'<input type="checkbox" name="variant-checkbox" id="variant-{table}:{self.longid}!checkbox">'
+            label_ele = f'<label for="variant-{table}:{self.longid}!checkbox">Variants</label>'
             return f"<br>{input_ele}\n{label_ele}"
         return ""
 
@@ -114,6 +139,7 @@ class Entry:
                 comment = comment.replace(f"[{ref}]", anchor)
         return comment
 
+    @cached_property
     def comment(self):
         comment_markdown = self.props.get("comment", "").rstrip()
         comment_html =  markdown.markdown(comment_markdown, 
@@ -135,30 +161,34 @@ class Attack(Entry):
     </tr>
     """
     template = Template("""
-    <tr id="attack-{{ id }}"
+    <tr id="attack:{{ this.longid }}"
         class="quantum-{{ quantum | default(false) }}
         complexity">
-    <td class="name">{{ this.name() }}</td>
-    <td class="complexity {{ this.complexity().simple() }}">{{ this.complexity() }}</td>
+    <td class="name">{{ this.name }}</td>
+    <td class="complexity {{ this.complexity.simple() }}">{{ this.complexity }}</td>
     <td class="quantum">{% if quantum %}Yes{% else %}No{% endif %}</td>
-    <td class="reference">{{ this.reference() }}</td>
+    <td class="reference">{{ this.reference }}</td>
     <td class="comment-checkbox">{{ this.comment_checkbox("attack") }}</td>
     </tr>
-    <tr id="comment-attack-{{ id }}" class="hidden-row">
-        <td colspan="5" class="comment-cell"><h4>Comment</h4>{{this.comment()}}</td>
+    <tr id="comment-attack:{{ this.longid }}" class="hidden-row">
+        <td colspan="5" class="comment-cell"><h4>Comment</h4>{{this.comment}}</td>
     </tr>
     """)
 
+    @property
     def quantum(self):
         return self.props.get("quantum", False)
 
+    @cached_property
     def complexity(self):
         return L.parse(self.props['complexity'])
 
 class Trivial(Attack):
     def __init__(self):
         self.id = 'trivial'
-        
+        self.props = { 'name': 'trivial' }
+
+    @property
     def complexity(self):
         return L(10)
 trivial = Trivial()
@@ -193,28 +223,30 @@ class Assumption(Entry):
     </tr>
     """
     template = Template("""
-    <tr id="assumption-{{ id }}">
-        <td class="name">{{ this.name() }}</td>
+    <tr id="assumption:{{ this.longid }}"
+        {% if this.parent %} class="hidden-row variant-row
+            variant-assumption:{{ this.parent.longid }}" {% endif %}>
+        <td class="name">{{ this.name }}</td>
         <td class="c_sec complexity {{ this.security(False).simple() }}">
-        <a href="#attack-{{ this.best_attack(False).props.id }}"
+        <a href="#attack:{{ this.best_attack(False).longid }}"
            title="{{ this.best_attack(False).props.name.long }}">{{ this.security(False) }}</a>
         </td>
         <td class="q_sec complexity {{ this.security().simple() }}">
-        <a href="#attack-{{ this.best_attack().props.id }}"
+        <a href="#attack:{{ this.best_attack().longid }}"
            title="{{ this.best_attack().props.name.long }}">{{ this.security() }}</a>
         </td>
-        <td class="reference">{{ this.reference() }}</td>
+        <td class="reference">{{ this.reference }}</td>
         <td class="checkboxes">
             {{ this.comment_checkbox("assumption") }}
             {{ this.variant_checkbox("assumption") }}
         </td>
     </tr>
-    <tr id="comment-assumption-{{ id }}" class="hidden-row">
-        <td colspan="5" class="comment-cell"><h4>Comment</h4>{{this.comment()}}</td>
+    <tr id="comment-assumption:{{ this.longid }}" class="hidden-row">
+        <td colspan="5" class="comment-cell"><h4>Comment</h4>{{this.comment}}</td>
     </tr>
     {% if this.props.variants %}
-        {% for variant, props in this.props.variants.items() %}
-            {{ this.get_variant(variant, props, id) }}
+        {% for variant in this.props.variants.values() %}
+            {{ variant }}
         {% endfor%}
     {% endif%}
     {% if this.props.variants|length % 2 == 1 %}
@@ -230,55 +262,31 @@ class Assumption(Entry):
         if 'reduces_to' in self.props:
             self.props['reduces_to'] = [assumptions[a]
                                         for a in self.props['reduces_to']]
+        if 'variants' in self.props:
+            for variant in self.props["variants"].values():
+                variant.link(assumptions, attacks)
 
     def best_attack(self, quantum=True, excl=None):
         ba1 = min((a
                    for a in self.props.get('attacks', [])
-                   if not a.quantum() or quantum),
-                  key=lambda a: a.complexity(),
+                   if not a.quantum or quantum),
+                  key=lambda a: a.complexity,
                   default=trivial)
         excl = ([] if excl is None else excl) + [self]
         ba2 = min((a.best_attack(quantum, excl)
                    for a in self.props.get('reduces_to', [])
                    if a not in excl),
-                  key=lambda a: a.complexity(),
+                  key=lambda a: a.complexity,
                   default=trivial)
         if ba1 is Trivial:
             return ba2
         elif ba2 is Trivial:
             return ba1
         else:
-            return min([ba1, ba2], key=lambda a: a.complexity())
+            return min([ba1, ba2], key=lambda a: a.complexity)
         
     def security(self, quantum=True):
-        return self.best_attack(quantum).complexity()
-
-    def get_variant(self, id, props, parent):
-        return AssumptionVariant(id, props, parent)
-
-class AssumptionVariant(Assumption):
-    def __init__(self, id, props, parent):
-        Assumption.__init__(self, id, props)
-        self.parent = parent
-
-    template = Template("""
-    <tr id="variant-assumption-{{ this.parent }}-{{ id }}" class="hidden-row variant-row variant-assumption-{{ this.parent }}">
-        <td class="name">{{ this.name() }}</td>
-        <td class="c_sec complexity {{ this.security(False).simple() }} ">
-        <a href="#attack-{{ this.best_attack(False).props.id }}"
-           title="{{ this.best_attack(False).props.name.long }}">{{ this.security(False) }}</a>
-        </td>
-        <td class="q_sec complexity {{ this.security().simple() }}">
-        <a href="#attack-{{ this.best_attack().props.id }}"
-           title="{{ this.best_attack().props.name.long }}">{{ this.security() }}</a>
-        </td>
-        <td class="reference">{{ this.reference() }}</td>
-        <td class="comment-checkbox">{{ this.comment_checkbox("assumption-" + this.parent) }}</td>
-    </tr>
-    <tr id="comment-assumption-{{ this.parent }}-{{ id }}" class="hidden-row">
-        <td colspan="5" class="comment-cell"><h4>Comment</h4>{{this.comment()}}</td>
-    </tr>
-    """)
+        return self.best_attack(quantum).complexity
 
 #---------------------------------------#
 # Logic for Schemes and Scheme variants #
@@ -296,29 +304,31 @@ class Scheme(Entry):
     </tr>
     """
     template = Template("""
-    <tr id="scheme-{{ id }}">
-        <td class="name">{{ this.name() }}</td>
+    <tr id="scheme:{{ this.longid }}"
+        {% if this.parent %} class="hidden-row variant-row
+            variant-scheme:{{ this.parent.longid }}" {% endif %}>
+        <td class="name">{{ this.name }}</td>
         <td class="type">{{ this.format_type() }}</td>
         <td class="c_sec complexity {{ this.security(False).simple() }}">
-        <a href="#attack-{{ this.best_attack(False).props.id }}"
+        <a href="#attack:{{ this.best_attack(False).longid }}"
            title="{{ this.best_attack(False).props.name.long }}">{{ this.security(False) }}</a>
         </td>
         <td class="q_sec complexity {{ this.security().simple() }}">
-        <a href="#attack-{{ this.best_attack().props.id }}"
+        <a href="#attack:{{ this.best_attack().longid }}"
            title="{{ this.best_attack().props.name.long }}">{{ this.security() }}</a>
         </td>
-        <td class="reference">{{ this.reference() }}</td>
+        <td class="reference">{{ this.reference }}</td>
         <td class="checkboxes">
             {{ this.comment_checkbox("scheme") }}
             {{ this.variant_checkbox("scheme") }}
         </td>
     </tr>
-    <tr id="comment-scheme-{{ id }}" class="hidden-row">
-        <td colspan="6" class="comment-cell"><h4>Comment</h4>{{this.comment()}}</td>
+    <tr id="comment-scheme:{{ this.longid }}" class="hidden-row">
+        <td colspan="6" class="comment-cell"><h4>Comment</h4>{{this.comment}}</td>
     </tr>
     {% if this.props.variants %}
-        {% for variant, props in this.props.variants.items() %}
-            {{ this.get_variant(variant, props, id) }}
+        {% for variant in this.props.variants.values() %}
+            {{ variant }}
         {% endfor%}
     {% endif%}
     {% if this.props.variants|length % 2 == 1 %}
@@ -330,14 +340,17 @@ class Scheme(Entry):
     def link(self, assumptions):
         self.props['assumptions'] = [assumptions[a]
                                      for a in self.props['assumptions']]
+        if 'variants' in self.props:
+            for variant in self.props["variants"].values():
+                variant.link(assumptions)
 
     def best_attack(self, quantum=True):
         return min((a.best_attack(quantum)
                     for a in self.props['assumptions']),
-                   key = lambda a: a.complexity())
+                   key = lambda a: a.complexity)
 
     def security(self, quantum=True):
-        return self.best_attack(quantum).complexity()
+        return self.best_attack(quantum).complexity
 
     def format_type(self):
         type_data = self.props['type']
@@ -345,69 +358,26 @@ class Scheme(Entry):
             return ", ".join(type_data)
         return type_data
 
-    def get_variant(self, id, props, parent):
-        return SchemeVariant(id, props, parent)
-
-class SchemeVariant(Scheme):
-    def __init__(self, id, props, parent):
-        Scheme.__init__(self, id, props)
-        self.parent = parent
-
-    template = Template("""
-    <tr id="variant-scheme-{{ this.parent }}-{{ id }}" class="hidden-row variant-row variant-scheme-{{ this.parent }}">
-        <td class="name">{{ this.name() }}</td>
-        <td class="type">{{ this.format_type() }}</td>
-        <td class="c_sec complexity {{ this.security(False).simple() }}">
-        <a href="#attack-{{ this.best_attack(False).props.id }}"
-           title="{{ this.best_attack(False).props.name.long }}">{{ this.security(False) }}</a>
-        </td>
-        <td class="q_sec complexity {{ this.security().simple() }}">
-        <a href="#attack-{{ this.best_attack().props.id }}"
-           title="{{ this.best_attack().props.name.long }}">{{ this.security() }}</a>
-        </td>
-        <td class="reference">{{ this.reference() }}</td>
-        <td class="comment-checkbox">{{ this.comment_checkbox("scheme-" + this.parent) }}</td>
-    </tr>
-    <tr id="comment-scheme-{{ this.parent }}-{{ id }}" class="hidden-row">
-        <td colspan="6" class="comment-cell"><h4>Comment</h4>{{this.comment()}}</td>
-    </tr>
-    """)
-
 #-----------------------------#
 # Logic to build the template #
 #-----------------------------#
-
-def create_classes_from_yml(yml_data, ClassBase, ClassVariant):
-    class_dict = {}
-    class_variants_dict = {}
-    for (id, props) in yaml.safe_load(yml_data).items():
-        class_dict[id] = ClassBase(id, props)
-        for (id_variant, props_variant) in props.get("variants", {}).items():
-            class_variants_dict[id_variant] = ClassVariant(id_variant, props_variant, id)
-    return class_dict, class_variants_dict
     
 with open('attacks.yml') as att:
     with open('assumptions.yml') as ass:
         with open('schemes.yml') as sch:
-            attacks = { id : Attack(id, props)
-                        for (id, props) in yaml.safe_load(att).items() }
-
-            # For the link, we now need to collect assumptions and variants
-            # This code got longer, so I factored it out for reuse for other
-            # Classes
-            assumptions, assumptions_variants = create_classes_from_yml(ass, Assumption, AssumptionVariant)
-            schemes, schemes_variants = create_classes_from_yml(sch, Scheme, SchemeVariant)
+            attacks = NestedDict({ id : Attack(id, props)
+                                   for (id, props) in yaml.safe_load(att).items() })
+            assumptions =  NestedDict({ id : Assumption(id, props)
+                                        for (id, props) in yaml.safe_load(ass).items() })
+            schemes =  NestedDict({ id : Scheme(id, props)
+                                    for (id, props) in yaml.safe_load(sch).items() })
             
             for a in assumptions.values():
                 a.link(assumptions, attacks)
-            for av in assumptions_variants.values():
-                av.link(assumptions, attacks)
             
             for s in schemes.values():
                 s.link(assumptions)
-            for sv in schemes_variants.values():
-                sv.link(assumptions)
-
+                
             env = Environment(
                 loader = FileSystemLoader("templates"),
                 autoescape=select_autoescape()
